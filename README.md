eef04148d9039825631708acfb66409e2e3f5849
Philipp Gassmann <phiphi@phiphi.ch>
2021-11-30 21:45:15 +0100

    add README and dev tooling

--- README.md
+++ README.md
@@ -1,20 +1,85 @@
 # TeamApps Emoji Icon Provider
 
-Open Source Emoji Icon sets:
+Use Emojis in your TeamApps Application UI
 
-* Twitter Twemoji https://twemoji.twitter.com/
-* OpenMoji Color and Line Style https://openmoji.org/library/
-* Mozilla FirefoxOS FxEmojis (incomplete, unmaintained): https://github.com/mozilla/fxemoji
+This library provides 1816 icons (3521 incl. skin tone variants) in 4 different styles.
+
+* Noto from Google
+* Twemoji From Twitter
+* OpenMoji Color
+* OpenMoji Black
+
+## Usage
+
+Add dependency to your TeamApps Maven Project:
+
+~~~xml
+        <dependency>
+            <groupId>org.teamapps</groupId>
+            <artifactId>teamapps-emoji-icon-provider</artifactId>
+            <version>1.0</version>
+        </dependency>
+~~~
+
+Usage:
+
+~~~java
+import org.teamapps.icon.emoji.EmojiIcon;
+import org.teamapps.icon.emoji.EmojiIconStyle;
+import org.teamapps.icons.Icon;
+
+public class IconExample {
+    public static Void someMethod() {
+        Icon smile = EmojiIcon.GRINNING_FACE;
+        Icon icon_with_skintone = EmojiIcon.RAISED_HAND__LIGHT_SKIN_TONE.withStyle(EmojiIconStyle.OPENMOJI_COLOR);
+        Icon flag = EmojiIcon.FLAG_GERMANY.withStyle(EmojiIconStyle.TWEMOJI);
+        
+        // Get Icon by Emoji as unicode String
+        Icon wavingHand = EmojiIcon.forUnicode("👋");
+    }
+}
+~~~
+
+## Emoji Icon Browser
+
+This Project includes a visual Icon Browser.
+
+![EmojiIconBrowser](./project-resources/EmojiIconBrowser.png)
+
+Launch it quickly from your Teamapps project.
+
+`Component emojiIconBrowser = new EmojiIconBrowser(sessionContext).getUI();`
+
+You can copy the following complete IconBrowser class. Run it and point your browser to <http://localhost:8082>
+
+~~~java
+import org.teamapps.icon.emoji.EmojiIconBrowser;
+import org.teamapps.server.jetty.embedded.TeamAppsJettyEmbeddedServer;
+import org.teamapps.ux.component.Component;
+import org.teamapps.ux.component.rootpanel.RootPanel;
+import org.teamapps.webcontroller.WebController;
+
+public class IconBrowser {
+    public static void main(String[] args) throws Exception {
+        WebController controller = sessionContext -> {
+            RootPanel rootPanel = new RootPanel();
+            sessionContext.addRootPanel(null, rootPanel);
+            Component emojiIconBrowser = new EmojiIconBrowser(sessionContext).getUI();
+            rootPanel.setContent(emojiIconBrowser);
+        };
+        new TeamAppsJettyEmbeddedServer(controller, 8082).start();
+    }
+}
+~~~
 
 ## SVG Sources
+
+This Icon Provider contains SVG Icons of following Open Source Emoji Icon sets:
 
 ### Noto SVG
 
 Adobe SVG Font Project https://github.com/adobe-fonts/noto-emoji-svg based on
 Google Font https://github.com/googlefonts/noto-emoji
-
-* Download Release Asset (Source code) from https://github.com/adobe-fonts/noto-emoji-svg
-* extract `svg` and `flag`
 
 Tools and most image resources are under the [Apache license, version 2.0](https://github.com/googlefonts/noto-emoji/blob/main/LICENSE). Flag images under third_party/region-flags are in the public domain or otherwise exempt from copyright (more info).
 
@@ -23,28 +88,77 @@ Tools and most image resources are under the [Apache license, version 2.0](https
 * https://openmoji.org/
 * https://emojis.wiki/openmoji/
 
-* Download and extract sets from: https://github.com/hfg-gmuend/openmoji/releases
-
-OpenMoji graphics are licensed under the Creative Commons Share Alike License 4.0 (CC BY-SA 4.0)
+OpenMoji graphics are licensed under the Creative Commons Share Alike License 4.0 [(CC BY-SA 4.0)](https://creativecommons.org/licenses/by-sa/4.0/)
 
 ### Twemoji from Twitter
 
-Twemoji https://twemoji.twitter.com/
+* https://twemoji.twitter.com/
 
-* Download Release Asset (Source code) from https://github.com/twitter/twemoji/releases
-* extract assets/svg
+Graphics licensed under [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/)
 
+#### Requirements (from Twemoji)
+
+##### Attribution
+
+As an open source project, attribution is critical from a legal, practical and motivational perspective in our opinion. The graphics are licensed under the CC-BY 4.0 which has a pretty good guide on best practices for attribution.
+
+However, we consider the guide a bit onerous and as a project, will accept a mention in a project README or an 'About' section or footer on a website. In mobile applications, a common place would be in the Settings/About section (for example, see the mobile Twitter application Settings->About->Legal section). We would consider a mention in the HTML/JS source sufficient also.
+
+##### Licensing
+
+Copyright 2020 Twitter, Inc and other contributors <br>
+Code licensed under the MIT License: http://opensource.org/licenses/MIT <br>
 Graphics licensed under CC-BY 4.0: https://creativecommons.org/licenses/by/4.0/
 
 ## General Information about Emoji 
-
-### Licensing
-* https://emojipedia.org/licensing/
-* http://unicode.org/emoji/images.html
-* https://blog.emojipedia.org/who-owns-emoji/
 
 ### Emoji Lists & Metadata
 
 * emoji-test.txt http://unicode.org/Public/emoji/
 * Complete Lists and Metadata https://github.com/Mange/emoji-data/
 * Java Library for Handling emoji in Text https://github.com/vdurmont/emoji-java/
+
+## Dev Notes
+
+### Get icon files
+
+The svg file assets are stored in [src/main/resources/org/teamapps/icon/emoji/](./src/main/resources/org/teamapps/icon/emoji/)
+
+* NOTO:
+  * Download Release Asset (Source code) from https://github.com/adobe-fonts/noto-emoji-svg
+  * extract `svg` and `flag`
+* TWEMOJI
+  * Download Release Asset (Source code) from https://github.com/twitter/twemoji/releases
+  * extract assets/svg
+* OPENMOJI
+  * Download and extract sets from: https://github.com/hfg-gmuend/openmoji/releases
+  
+### generate EmojiIcon Entries
+
+The EmojiIcon entries are generated based on [emoji-test.txt](project-resources/emoji-test.txt) using a python script
+
+The [emoji-test.txt](project-resources/emoji-test.txt) file was obtained from http://unicode.org/Public/emoji/
+
+~~~bash
+cd project-resources
+python3 generate_java_entries.py -J
+~~~
+
+Then copy the entries from [emoji.java](project-resources/emoji.java) to [EmojiIcon.java](src/main/java/org/teamapps/icon/emoji/EmojiIcon.java)
+
+
+
+## TEAMAPPS SOFTWARE LICENSE
+
+Apache 2.0
+
+## ICONS LICENSE
+
+The graphics are licenced under a free license, but some require attribution.
+
+
+### Emoji Licensing
+
+* https://emojipedia.org/licensing/
+* http://unicode.org/emoji/images.html
+* https://blog.emojipedia.org/who-owns-emoji/
---
+++ project-resources/generate_java_entries.py
@@ -0,0 +1,191 @@
+# Copyright © 2021 Teamapps.org
+# Licensed under the Apache License, Version 2.0
+"""
+Generates Entries for EmojiIconProvider or CSV with emoji data
+"""
+import argparse
+import io
+import os
+import sys
+import unicodedata
+import re
+
+REG_IND_LETTR = ('1F1E6 1F1E7 1F1E8 1F1E9 1F1EA 1F1EB 1F1EC 1F1ED '
+                 '1F1EE 1F1EF 1F1F0 1F1F1 1F1F2 1F1F3 1F1F4 1F1F5 '
+                 '1F1F6 1F1F7 1F1F8 1F1F9 1F1FA 1F1FB 1F1FC 1F1FD '
+                 '1F1FE 1F1FF').split()
+
+TAG_LAT_LETTR = ('E0061 E0062 E0063 E0064 E0065 E0066 E0067 E0068 E0069 E006A '
+                 'E006B E006C E006D E006E E006F E0070 E0071 E0072 E0073 E0074 '
+                 'E0075 E0076 E0077 E0078 E0079 E007A').split()
+
+SKIP_STATUSES = ('unqualified', 'non-fully-qualified', 'minimally-qualified')
+
+FILE_PREFIX = 'u'
+
+WORKING_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_INPUT_PATH = os.path.join(WORKING_DIR, 'emoji-test.txt')
+
+
+def strip_accents(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                    if unicodedata.category(c) != 'Mn')
+
+
+def append_to_file(fpath, data, enc='utf-8'):
+    with io.open(fpath, 'a', encoding=enc) as fp:
+        fp.write(data)
+
+def init_file(fpath, data, enc='utf-8'):
+    with io.open(fpath, 'w', encoding=enc) as fp:
+        fp.write(data)
+
+def parse_emoji_test_file(filename):
+    """
+    Parses Unicode's 'emoji-test.txt' file (available from
+    http://unicode.org/Public/emoji/M.m/ where 'M.m' is the version number)
+    and returns a list of code points.
+    """
+    with io.open(filename, encoding='utf-8') as fp:
+        lines = fp.read().splitlines()
+
+    emoji_list = []
+    for line in lines:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+        codepoints, status_emoname = line.split(';')
+        status = status_emoname.split('#', 1)[0].strip()
+        emo_info = status_emoname.split('#', 1)[1].strip()
+        emo_unicode = emo_info.split(' ')[0].strip()
+        emo_version = emo_info.split(' ')[1].strip()
+        emo_name = emo_info.split(emo_version)[1].strip()
+        emo_data = [emo_unicode, emo_name, emo_version]
+        # print('; '.join(emo_data))
+        if status in SKIP_STATUSES:
+            continue
+        cdpts = codepoints.strip().split();
+        emoji_list.append([cdpts, emo_data])
+    return emoji_list
+
+
+def emoji_data(emoji):
+    cps = emoji[0]
+    emoji_data = emoji[1]
+    emoji_unicode =  emoji_data[0]
+    emoji_name = emoji_data[1]
+    # determine if it's a country/regional flag
+    is_flag = False
+    if len(cps) > 1 and cps[1] in (REG_IND_LETTR + TAG_LAT_LETTR):
+        is_flag = True
+
+    cps_html = ''.join('&#x{};'.format(cp) for cp in cps)
+
+    # filenames have no 'FE0F' or 'E007F' components
+    cps_filename = [cp for cp in cps if cp not in ('FE0F', 'E007F')]
+    filename = FILE_PREFIX + '_'.join(cps_filename).lower()
+
+    png_dir = 'flags_png' if is_flag else 'png'
+    svg_dir = 'flags' if is_flag else 'svg'
+    sbw_dir = 'flags_bw' if is_flag else 'svg_bw'
+
+    png_file = "{}/{}.png".format(png_dir, filename)
+    svg_file = "{}/{}.svg".format(svg_dir, filename)
+    sbw_file = "{}/{}.svg".format(sbw_dir, filename)
+
+    base_name = emoji_name.split(':', 1)[0].strip()
+    try:
+        variant = emoji_name.split(':', 1)[1].strip()
+        safe_name = "{}__{}".format(base_name, variant)
+    except IndexError:
+        variant = ''
+        safe_name = base_name
+
+
+
+    if is_flag:
+        flag_name = emoji_name.split(':', 1)[1].strip()
+        safe_country = strip_accents(flag_name)
+        base_name = flag_name
+        safe_name = "flag_{}".format(safe_country)
+        variant = ''
+
+    safe_name = re.sub(r' ', '_', safe_name)
+    safe_name = re.sub(r'#', 'hashtag', safe_name) # keycode #
+    safe_name = re.sub(r'[*]', 'asterisk', safe_name) # keycode *
+    safe_name = re.sub(r'ñ', 'n', safe_name) # piñata
+    safe_name = re.sub(r'(.*)_medal', r'medal_\1', safe_name) # beginning with number
+    safe_name = re.sub(r'[\W+]', '', safe_name).lower()
+
+    # return [i, emoji_unicode, safe_name, emoji_name, base_name, variant, '_'.join(cps), cps_html, filename, png_file, svg_file, sbw_file]
+    return {
+        'emoji_unicode': emoji_unicode,
+        'safe_name': safe_name,
+        'emoji_name': emoji_name,
+        'base_name': base_name,
+        'variant': variant,
+        'cps': cps,
+        'cps_html': cps_html,
+        'filename': filename,
+        'png_file': png_file,
+        'svg_file': svg_file,
+        'sbw_file': sbw_file,
+        'is_flag' : is_flag
+    }
+
+
+def write_csv(emoji_list):
+    csv_file = os.path.join(WORKING_DIR, '..', 'emoji.csv')
+    # csv_header = '\t'.join(["emoji_unicode", "emoji_name", "codepoints_", "cps_html", "png_file", "svg_file", "sbw_file"])+"\n"
+    csv_header = '"number", "emoji_unicode", "safe_name", "emoji_name", "base_name", "variant",  "codepoints_", "cps_html", "png_file", "svg_file", "sbw_file" \n'
+    init_file(csv_file, csv_header)
+    for i, emoji in enumerate(emoji_list, 1):
+        emo_data = emoji_data(emoji)
+        #csv_line = '"{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}" \n'.format(i, emoji_unicode, safe_name, emoji_name, base_name, variant, '_'.join(cps), cps_html, png_file, svg_file, sbw_file)
+        csv_line = '"{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}" \n'.format(i, emo_data['emoji_unicode'], emo_data['safe_name'], emo_data['emoji_name'], emo_data['base_name'], emo_data['variant'], '_'.join(emo_data['cps']), emo_data['cps_html'], emo_data['filename'], emo_data['png_file'], emo_data['svg_file'], emo_data['sbw_file'])
+        # csv_line = emo_data["safe_name"]
+        append_to_file(csv_file, csv_line)
+
+def write_java(emoji_list):
+    java_file = os.path.join(WORKING_DIR, 'emoji.java')
+    java_header = '        // generated entries \n'
+    init_file(java_file, java_header)
+    for i, emoji in enumerate(emoji_list, 1):
+        emo_data = emoji_data(emoji)
+        #csv_line = '"{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}" \n'.format(i, emoji_unicode, safe_name, emoji_name, base_name, variant, '_'.join(cps), cps_html, png_file, svg_file, sbw_file)
+        # csv_line = '"{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}" \n'.format(i, emo_data['emoji_unicode'], emo_data['safe_name'], emo_data['emoji_name'], emo_data['base_name'], emo_data['variant'], '_'.join(emo_data['cps']), emo_data['cps_html'], emo_data['filename'], emo_data['png_file'], emo_data['svg_file'], emo_data['sbw_file'])
+        # csv_line = emo_data["safe_name"]
+        # append_to_file(csv_file, csv_line)
+        # java_line = 'public static final EmojiIcon {} = create("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}");\n'.format(safe_name.upper(), i, emoji_unicode, safe_name, emoji_name, base_name, variant, '_'.join(cps), cps_html, png_file, svg_file, sbw_file)
+        is_flag_java_boolean = "true" if emo_data['is_flag'] else "false"
+        java_line = '        public static final EmojiIcon {} = create({}, "{}", "{}", {});\n'.format(emo_data['safe_name'].upper(), i, emo_data['emoji_unicode'], emo_data['safe_name'].upper(), is_flag_java_boolean)
+        append_to_file(java_file, java_line)
+
+def main(args=None):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '-C',
+        '--csv',
+        help="generates csv instead of html",
+        action='store_true',
+    )
+    parser.add_argument(
+        '-J',
+        '--java',
+        help="generates java",
+        action='store_true',
+    )
+    opts = parser.parse_args(args)
+
+    emoji_list = parse_emoji_test_file(TEST_INPUT_PATH)
+
+    if opts.csv:
+        write_csv(emoji_list)
+    elif opts.java:
+        write_java(emoji_list)
+    else:
+        write_java(emoji_list)
+
+if __name__ == "__main__":
+    sys.exit(main())
